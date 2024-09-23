c7744c56b285d31b9ba9f442ddbdc5b3ece1d2ed
HenriqueRicardoFigueira <henrislip@gmail.com>
2024-09-22 21:26:09 -0300

    finish crud

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,15 +4,10 @@ import (
 	"alura_go/database"
 	"alura_go/models"
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
-}
 
 func GetAllPersonalities(w http.ResponseWriter, r *http.Request) {
 	var allPersonalities []models.Personality
@@ -27,5 +22,35 @@ func GetPersonality(w http.ResponseWriter, r *http.Request) {
 	var personality models.Personality
 
 	database.DB.First(&personality, id)
+	json.NewEncoder(w).Encode(personality)
+}
+
+func DeletePersonality(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personality models.Personality
+
+	database.DB.Delete(&personality, id)
+	json.NewEncoder(w).Encode(personality)
+}
+
+func EditPersonality(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personality models.Personality
+
+	database.DB.First(&personality, id)
+	json.NewDecoder(r.Body).Decode(&personality)
+	database.DB.Save(&personality)
+
+	json.NewEncoder(w).Encode(personality)
+}
+
+func CreatePersonality(w http.ResponseWriter, r *http.Request) {
+	var personality models.Personality
+	json.NewDecoder(r.Body).Decode(&personality)
+
+	database.DB.Create(&personality)
+
 	json.NewEncoder(w).Encode(personality)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -2,16 +2,21 @@ package routes
 
 import (
 	"alura_go/controllers"
+	"alura_go/middleware"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequests() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", controllers.Home)
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/api/personalities", controllers.GetAllPersonalities).Methods("Get")
 	r.HandleFunc("/api/personalities/{id}", controllers.GetPersonality).Methods("Get")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	r.HandleFunc("/api/personalities/{id}", controllers.EditPersonality).Methods("Put")
+	r.HandleFunc("/api/personalities/{id}", controllers.DeletePersonality).Methods("Delete")
+	r.HandleFunc("/api/personalities", controllers.CreatePersonality).Methods("Post")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
